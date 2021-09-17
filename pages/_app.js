58e4ce40ad9362495341f9cce2079dbb16d178fe
jonatas de souza mattos <jonidefjan@gmail.com>
2021-09-17 14:12:34 -0300

    0.1.1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-// add bootstrap css 
-import 'bootstrap/dist/css/bootstrap.css'
-// own css files here
+import Head from "next/head";
 import "../css/customcss.css";
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+return (
+<>
+<Head>
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+</Head>
+<Component {...pageProps} />
+</>
+);
 }
+export default MyApp;
